20a139ad80d1c864a2f59baac6350cadaf5f810b
skippa <881423@stud.unive.it>
2023-04-23 18:18:01 +0200

    go

--- src/model/dataManager.py
+++ src/model/dataManager.py
@@ -1,17 +1,16 @@
 import json
-
 from nltk import word_tokenize
 from numpy import ndarray, array, append
 from pandas import DataFrame, read_csv
-import nltk
+from rank_bm25 import BM25Okapi
 
 
 class DataManager(object):
     def __init__(self, queryPath: str, documentsPath: str, solutionPath):
         self.queries: ndarray[dict[str, str]] = array([])
         self.documents: ndarray[dict[str, str]] = array([])
-        self.tokenizedQueries: ndarray[list[str]] = array([])
-        self.tokenizedDocuments: ndarray[list[str]] = array([])
+        self.tokenizedQueries: dict[str, list[str]] = {}
+        self.tokenizedDocuments: dict[str, list[str]] = {}
 
         self.solutions: DataFrame = read_csv(solutionPath)
         self._readJson(queryPath)
@@ -39,14 +38,24 @@ class DataManager(object):
 
     def _tokenize(self):
         for query in self.queries:
-            self.tokenizedQueries = append(self.tokenizedQueries, word_tokenize(query["text"]))
+            self.tokenizedQueries[query["_id"]] = word_tokenize(query["text"])
 
         for doc in self.documents:
-            self.tokenizedDocuments = append(self.tokenizedDocuments, word_tokenize(doc["title"] + doc["text"]))
+            self.tokenizedDocuments[doc["_id"]] = word_tokenize(doc["title"] + " " + doc["text"])
+
+    def _getSparseScores(self) -> dict[str, ndarray[tuple[str, float]]]:
+        bm25 = BM25Okapi(list(self.tokenizedDocuments.values()))
+        solution: dict[str, ndarray[tuple[str, float]]] = {}
+        documentKeys = list(self.tokenizedDocuments.keys())
+        for key in self.tokenizedQueries:
+            scores: ndarray[float] = array(bm25.get_scores(self.tokenizedQueries[key]))
+            solution["query{}".format(key)] = array([(documentKeys[idx], scores[idx]) for idx in range(0, len(scores))])
+
+        return solution
 
 
-base: str = "C:\\Users\\biagi\\OneDrive\\Documents\\Desktop\\trec-covid\\"
+base: str = "data/"
 
-manager: DataManager = DataManager(base + "queries.jsonl", base + "corpus.jsonl", base + "qrels\\test.tsv")
+manager: DataManager = DataManager(base + "queries.jsonl", base + "corpus.jsonl", base + "qrels/test.tsv")
 
 print(manager.queries)
